f6de9adbcf23f4d1d21a0837383eb4566d8467a6
359442094 <359442094@qq.com>
2018-12-11 16:40:48 +0800

    前后端互传JSON

--- src/main/java/sk/springboot/controller/DeptController.java
+++ src/main/java/sk/springboot/controller/DeptController.java
@@ -3,8 +3,7 @@ package sk.springboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import sk.springboot.entity.Dept;
@@ -28,6 +27,31 @@ public class DeptController {
         maps.put("depts", deptService.selectDepts());
         model.addAttribute("maps",maps);
         return "index";
+    }
+
+    /**
+     * 接收前端的JSON参数
+     * */
+    @ResponseBody
+    @RequestMapping("/getJSON")
+    public String getJSON(@RequestBody String vars){
+        System.out.println("前端传值的参数为:");
+        System.out.println(vars);
+        return "result";
+    }
+
+    /**
+     * 返回JSON格式字符串
+     * */
+    @ResponseBody
+    @RequestMapping("/showJSON")
+    public Object showJSON(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("cfInstId","分配的商户号");
+        map.put("sign","报文主体签名信息");
+        map.put("encryptedMsg","报文密文");
+        map.put("encryptedSk","会话密钥密文");
+        return map;
     }
 
 }
